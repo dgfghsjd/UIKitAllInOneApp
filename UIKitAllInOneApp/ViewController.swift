073cfe4460d763389928718629850c619da4b975
dgfghsjd <dgfghsjd@naver.com>
2023-05-27 17:47:21 +0900

    add basic

--- UIKitAllInOneApp/ViewController.swift
+++ UIKitAllInOneApp/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var phoneLabel: UILabel!
 
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var phoneField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameField.placeholder = "이름을 입력해주세요"
+        phoneField.placeholder = "010-XXXX-XXXX"
     }
 
 
